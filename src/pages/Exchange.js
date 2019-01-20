82e35b574ac73e925575ef7c6e605c4234f9ad9b
송동욱 <quino0627@korea.ac.kr>
2019-01-20 16:30:04 +0900

    .

---
+++ src/pages/Exchange.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Exchange = () => {
+    return (
+        <div>
+            <h2>Exchange</h2>
+        </div>
+    );
+};
+
+export default Exchange;
---
+++ src/pages/index.js
@@ -0,0 +1,9 @@
+export { default as Home } from './Home';
+export { default as Chat } from './Chat';
+export { default as Exchange } from './Exchange';
+export { default as Food } from './Food';
+export { default as Notice } from './Notice';
+export { default as Review } from './Review';
+export { default as Login } from './Login';
+
+// 컴포넌트를 불러와서 한 파일로 내보낼 수 있도록 인덱스
